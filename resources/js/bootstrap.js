2647adfa5e4b44641b746afdae70ccc8f8bd0352
Azim Sofi <azimmiskandar@gmail.com>
2024-08-11 13:17:24 +0900

    Template

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,6 @@
-import axios from 'axios';
-window.axios = axios;
+import 'bootstrap';
+import '@popperjs/core';  // Import Popper.js, required by Bootstrap
 
+import axios from 'axios';  // Your existing axios setup
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
