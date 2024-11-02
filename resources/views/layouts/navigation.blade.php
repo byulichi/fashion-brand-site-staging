18f52bbe874880c3d088cf7a48647f6288b11e24
Azim Sofi <azimmiskandar@gmail.com>
2024-11-02 13:45:27 +0900

    管理

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,6 @@
 <!-- Header -->
 <header class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <?php session(['url.intended' => url()->full()]); ?>
     <div class="container-fluid my-2">
         <a class="navbar-brand" href="{{ url('/') }}">
             <img src="#" height="40" alt="Brand Logo">
@@ -9,114 +10,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mx-auto"> <!-- Centered Navigation -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="newInDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        New In
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="newInDropdown">
-                        <li><a class="dropdown-item" href="#">Item 1</a></li>
-                        <li><a class="dropdown-item" href="#">Item 2</a></li>
-                        <li><a class="dropdown-item" href="#">Item 3</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="scarvesDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Scarves
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="scarvesDropdown">
-                        <li><a class="dropdown-item" href="#">Combo Deals</a></li>
-                        <li><a class="dropdown-item" href="#">Plain Square</a></li>
-                        <li><a class="dropdown-item" href="#">Printed Square</a></li>
-                        <li><a class="dropdown-item" href="#">Plain Shawl</a></li>
-                        <li><a class="dropdown-item" href="#">Printed Shawl</a></li>
-                        <li><a class="dropdown-item" href="#">Capsule Edition</a></li>
-                        <li><a class="dropdown-item" href="#">Printed Instant</a></li>
-                        <li><a class="dropdown-item" href="#">Plain Instant</a></li>
-                        <li><a class="dropdown-item" href="#">Tiara</a></li>
-                        <li><a class="dropdown-item" href="#">Wide Square</a></li>
-                        <li><a class="dropdown-item" href="#">Packaging</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="readyToWearDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Ready To Wear
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="readyToWearDropdown">
-                        <li><a class="dropdown-item" href="#">Item 1</a></li>
-                        <li><a class="dropdown-item" href="#">Item 2</a></li>
-                        <li><a class="dropdown-item" href="#">Item 3</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="collaborationsDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Collaborations
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="collaborationsDropdown">
-                        <li><a class="dropdown-item" href="#">Item 1</a></li>
-                        <li><a class="dropdown-item" href="#">Item 2</a></li>
-                        <li><a class="dropdown-item" href="#">Item 3</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="aryanDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Aryan
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="aryanDropdown">
-                        <li><a class="dropdown-item" href="#">Item 1</a></li>
-                        <li><a class="dropdown-item" href="#">Item 2</a></li>
-                        <li><a class="dropdown-item" href="#">Item 3</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="LuxeDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Luxe
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="LuxeDropdown">
-                        <li><a class="dropdown-item" href="#">Item 1</a></li>
-                        <li><a class="dropdown-item" href="#">Item 2</a></li>
-                        <li><a class="dropdown-item" href="#">Item 3</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="prayerwearDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Suit
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="prayerwearDropdown">
-                        <li><a class="dropdown-item" href="{{ route('products', ['type' => 'Leena Suit']) }}">Leena suit</a></li>
-                        <li><a class="dropdown-item" href="{{ route('products', ['type' => 'Tasneem Suit']) }}">Taneem suit</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="bagsDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Bags
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="bagsDropdown">
-                        <li><a class="dropdown-item" href="#">Item 1</a></li>
-                        <li><a class="dropdown-item" href="#">Item 2</a></li>
-                        <li><a class="dropdown-item" href="#">Item 3</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="accessoriesAndGiftingDropdown"
-                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Accessories & Gifting
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="accessoriesAndGiftingDropdown">
-                        <li><a class="dropdown-item" href="#">Item 1</a></li>
-                        <li><a class="dropdown-item" href="#">Item 2</a></li>
-                        <li><a class="dropdown-item" href="#">Item 3</a></li>
-                    </ul>
-                </li>
-            </ul>
+            @include("layouts.navigationitem")
             <ul class="navbar-nav ms-auto">
                 @if (Route::has('login'))
                     @auth
