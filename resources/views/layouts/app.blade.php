2b2c63bc4fa384694a080bc769f3e688f5eb803d
Azim Sofi <azimmiskandar@gmail.com>
2025-01-08 16:48:13 +0900

    Update footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,31 +1,35 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-white dark:bg-gray-900">
-            @include('layouts.navigation')
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-        <!-- Footer -->
-        <footer class="bg-white text-center py-3 mt-5">
-            <p>&copy; 2024 Your Clothing Store. All rights reserved.</p>
-        </footer>
-    </body>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased flex flex-col min-h-screen">
+    <div class="bg-white dark:bg-gray-900 flex-grow">
+        @include('layouts.navigation')
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+    <!-- Footer -->
+    <footer class="d-flex align-items-center justify-content-center py-4 mt-auto border-t-2 w-full"
+        style="background-color: #F1F1F1">
+    <p class="text-black m-0"> Copyright © 2025, Fashion Clothing Store. All Rights Reserved.</p>
+</footer>
+</body>
+
 </html>
