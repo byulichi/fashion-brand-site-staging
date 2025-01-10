fdc8797c17eadb767bcf0134bf4457f742ccaeea
Azim Sofi <azimmiskandar@gmail.com>
2025-01-10 22:44:39 +0900

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,11 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
 import jQuery from 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import * as Popper from '@popperjs/core';
 
 window.Alpine = Alpine;
 window.$ = window.jQuery = jQuery;
+window.Popper = Popper;
 
 Alpine.start();
